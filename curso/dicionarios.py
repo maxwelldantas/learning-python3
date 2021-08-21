09564fcf75e995da31e06f5d9ec9c7630fceed00
Maxwell Dantas <mdszdev@gmail.com>
2021-08-21 19:01:55 -0300

    Aula 9 aprendendo sobre dicionários

---
+++ curso/dicionarios.py
@@ -0,0 +1,19 @@
+# Listas tipo chave:valor - dict(dicionário)
+
+# Criando um dicionário vazio
+livros = {}
+
+# Forma de adicionar chave e valor no dicionário
+livros['titulo'] = 'Aprenda a programar em python em 6hs'
+livros['ano'] = 2018
+
+# Criando um dicionário com chaves e valores
+pessoa = {'nome': 'José da Silva', 'idade': 30, 'altura': 1.8, 'peso': 93}
+'''
+ Os métodos do dicionário são parecidos com uma função, 
+ possuem parâmetros e retornam valores, mas a sintaxe é diferente.
+'''
+
+pessoa.keys()  # Retorna a lista com os índices
+
+pessoa.values()  # Retorna a lista com os valores
