26e3eeffe321e1d71fafee40d6c59b810fd54de3
Maxwell Dantas <mdszdev@gmail.com>
2021-08-21 23:49:45 -0300

    Aula 13 aprendendo sobre operadores de comparação e condicionais

---
+++ curso/operadoresComparacaoCondicionais.py
@@ -0,0 +1,51 @@
+# Operadores de comparação
+# > maior que
+# < menor que
+# >= maior ou igual
+# <= menor ou igual
+# == igual
+# != não igual
+# and  e
+# or   ou
+
+
+# Condicionais - if, elif, else
+
+# if (condição):
+#     código executado se for verdadeira a condição
+# else:
+#     código executado se for falsa a condição
+
+
+# x = 7
+#
+# if x % 2 == 0:
+#     print(x, 'é um número par')
+# else:
+#     print(x, 'é um número ímpar')
+
+
+# a = 30
+# b = 20
+#
+# if a > b:
+#     print('a é maior que b')
+# elif a == b:
+#     print('a é igual a b')
+# else:
+#     print('a é menor que b')
+
+
+# Condições para entrar no cinema:
+#  Pagar ingresso e ser maior de 18 anos
+
+# pagou_ingresso = True
+# maior_de_dezoito = False
+#
+# if pagou_ingresso or maior_de_dezoito:
+# if pagou_ingresso and maior_de_dezoito:
+#     print('Pode entrar')
+# else:
+#     print('Não pode entrar')
+
+
