0c010ca9fb0c31f3786e6d04bb42c41ff1683020
Maxwell Dantas <mdszdev@gmail.com>
2021-08-28 15:03:06 -0300

    Aula 26 aprendendo sobre funções

---
+++ paradigma_orientacao_a_objetos/funcao.py
@@ -0,0 +1,17 @@
+# Uma função é definida pelas inicias def que significa Definições de função
+
+def soma(x, y):
+    resultado = x + y
+    print(resultado)
+
+
+# soma(8, 9)
+
+
+def subtracao(a, b):
+    return a - b
+
+
+resultado = subtracao(19, 9)
+
+print(resultado)
